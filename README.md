fa596fdcc30f40306b3207d4b2b28f13858a0dba
Dominik Wilkowski <hi@dominik-wilkowski.com>
2020-10-28 13:16:14 +1100

    added light and dark mode detection and listener

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Asterisk.art App
 
+<p align="center">
+	<a href="https://circleci.com/gh/asterisk-art/app/tree/main" rel="nofollow">
+		<img src="https://circleci.com/gh/asterisk-art/app/tree/main.svg?style=svg">
+	</a>
+</p>
+
 > Create art with algorithms
--- src/App.js
+++ src/App.js
@@ -2,12 +2,29 @@
 /** @jsx jsx */
 
 import { jsx, Global } from '@emotion/core';
-import { Fragment, useState } from 'react';
+import { Fragment, useState, useEffect } from 'react';
 
 import COLORS from './tokens/colors.json';
 
 export function App() {
-	const [theme, setTheme] = useState('light');
+	const [theme, setTheme] = useState(
+		window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+	);
+
+	useEffect(() => {
+		const modeListener = (event) => {
+			if (event.matches) {
+				setTheme('dark');
+			} else {
+				setTheme('light');
+			}
+		};
+		window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', modeListener);
+
+		return window
+			.matchMedia('(prefers-color-scheme: dark)')
+			.removeEventListener('change', modeListener);
+	}, []);
 
 	return (
 		<Fragment>
