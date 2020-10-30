540be0ce5b3e15025f0a3dd2e2a89978b03b4b07
Dominik Wilkowski <hi@dominik-wilkowski.com>
2020-10-31 09:59:07 +1100

    added version to about page

--- src/About.js
+++ src/About.js
@@ -3,10 +3,13 @@
 
 import { jsx } from '@emotion/core';
 
+import { version } from '../package.json';
+
 export function About() {
 	return (
 		<div>
 			<h1>About</h1>
+			v{version}
 		</div>
 	);
 }
